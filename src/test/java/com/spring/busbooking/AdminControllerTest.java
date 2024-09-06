1ea137ed53a14b8453db206150b4432722a7ed59
Dheebiga-R <113468871+Dheebiga-R@users.noreply.github.com>
2024-09-06 08:19:15 +0530

    Update AdminControllerTest.java

--- src/test/java/com/spring/busbooking/AdminControllerTest.java
+++ src/test/java/com/spring/busbooking/AdminControllerTest.java
@@ -1,128 +1,104 @@
 package com.spring.busbooking;
-
-import com.spring.busbooking.controller.AdminController;
-import com.spring.busbooking.model.Bus;
-import com.spring.busbooking.repository.BusRepository;
-import com.spring.busbooking.service.AdminService;
-import com.spring.busbooking.validation.BusNotFoundException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
+import java.util.Optional;
 
-@WebMvcTest(AdminController.class)
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.spring.busbooking.model.Passenger;
+import com.spring.busbooking.repository.PassengerRepository;
+
+import com.spring.busbooking.service.PassengerServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
 public class AdminControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Mock
-    private BusRepository busRepository;
+		private PassengerRepository passengerRepository;
+		
+		@InjectMocks
+		private PassengerServiceImpl passengerServiceImpl;
+		
+		private Optional<Passenger> passenger;
+		
+		@BeforeEach
+		public void setup() {
+			passenger = Optional.of(Passenger.builder()
+					.id(1)
+					.age(45)
+					.contactDetails("3452678532")
+					.name("dheebiga")
+					.build());
+			
+		}
+		
+		
+		@DisplayName("Junit test for find passenger by id")
+		@Test
+		public void findPassengerById_foundReturn() {
+			given(passengerRepository.findById(passenger.get().getId()))
+			.willReturn(passenger);
+			Passenger passenger2 = passengerServiceImpl.findPassengerById(passenger.get().getId());
+			assertThat(passenger2).isNotNull();
+		}
+		
+		@DisplayName("JUnit test for savePassenger method which throws exception")
+	    @Test
+	    public void givenExistingId_whenSavePassenger_thenThrowsException(){
+	        // given - precondition or setup
+	        given(passengerRepository.findById(passenger.get().getId()))
+	                .willReturn(passenger);
 
-    @Mock
-    private AdminService adminService;
+	        System.out.println(passengerRepository);
+	        System.out.println(passengerServiceImpl);
 
-    @InjectMocks
-    private AdminController adminController;
+	        // when -  action or the behaviour that we are going test
+	        org.junit.jupiter.api.Assertions.assertThrows(Exception.class, () -> {
+	            passengerServiceImpl.savePassenger(passenger);
+	        });
 
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
-    }
+	        // then
+	        verify(passengerRepository, never()).save(any(Passenger.class));
+	    }
+		
+		@DisplayName("JUnit test for getAllPassengers method")
+	    @Test
+	    public void givenPassengersList_whenGetAllPassengers_thenReturnPassengerList(){
+	        // given - precondition or setup
 
-    @Test
-    public void testGetmethod() throws Exception {
-        // Arrange
-        when(adminService.getAllBuses()).thenReturn(List.of(new Bus()));
+	        Passenger passenger1 = Passenger.builder()
+	        		             .id(2)
+	        		             .age(23)
+	        		             .contactDetails("9844567321")
+	        		             .name("Gurunath")
+	        		             .build();
+	        
+	        Passenger passenger2 = Passenger.builder()
+		             .id(1)
+		             .age(43)
+		             .contactDetails("9944567321")
+		             .name("Ashok")
+		             .build();
 
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.get("/admin")
-                .accept(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("admin"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("buses"))
-                .andDo(print());
-    }
+	        given(passengerRepository.findAll()).willReturn(List.of(passenger1, passenger2));
 
-    @Test
-    public void testAddRoute() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/admin/add")
-                .accept(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("admin_add_route"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("bus"))
-                .andDo(print());
-    }
+	        // when -  action or the behaviour that we are going test
+	        List<Passenger> PassengerList = passengerServiceImpl.getAllPassengers();
 
-    @Test
-    public void testProcessRoute() throws Exception {
-        Bus bus = new Bus();
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.post("/admin/add")
-                .flashAttr("bus", bus)
-                .param("busName", "Bus Name")
-                .param("busClass", "Luxury")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/admin"))
-                .andDo(print());
-
-        verify(busRepository, times(1)).save(any(Bus.class));
-    }
-
-    @Test
-    public void testDeleteRoute() throws Exception {
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.get("/admin/delete/1"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/admin"))
-                .andDo(print());
-
-        verify(busRepository, times(1)).deleteById(1);
-    }
-
-    @Test
-    public void testUpdateRoute() throws Exception {
-        when(adminService.findBusById(1)).thenReturn(new Bus());
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/admin/update/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("admin_update_route"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("bus"))
-                .andDo(print());
-    }
-
-    @Test
-    public void testUpdateProcess() throws Exception {
-        Bus bus = new Bus();
-        when(adminService.findBusById(1)).thenReturn(bus);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/admin/update/1")
-                .flashAttr("bus", bus)
-                .param("busName", "Updated Bus Name")
-                .param("busClass", "Luxury")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/admin"))
-                .andDo(print());
-
-        verify(adminService, times(1)).updateBus(any(Bus.class));
-    }
+	        // then - verify the output
+	        assertThat(PassengerList).isNotNull();
+	        assertThat(PassengerList.size()).isEqualTo(2);
+	    }
 }
 
